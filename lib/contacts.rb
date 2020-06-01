4f7fb928a52c2c3fa1ee197340a3be9a6bf804bf
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-01 19:08:43 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -4,7 +4,7 @@ def remove_strawberry(contacts)
 
   contacts.each do |person, contact_details_hash|
       if person == "Freddy Mercury"
-        contact_details_each.each do |attribute, data|
+        contact_details_each.hash do |attribute, data|
       binding.pry
     end
   end
